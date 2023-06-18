6fbf841561016a5a12ce5b9451cae3574312360d
Konvoir <60233103+Konvoir@users.noreply.github.com>
2023-06-18 18:28:54 +0300

    books

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
 
 function App() {
+const [booksData, setBooksData] = React.useState([])
+
+React.useEffect(() =>{
+
+},[])
+
+async function getData(){
+  await axios
+}
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+    <div className="app">
+    <div className="app__body">
+    
+
+
+    </div>
+    </div>
     </div>
   );
 }
