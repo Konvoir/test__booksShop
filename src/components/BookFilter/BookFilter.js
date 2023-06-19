2178dc7283584a9049d83b40e263a89b09e722b6
Konvoir <baleskoff@gmail.com>
2023-06-19 10:47:28 +0300

    sort

--- src/components/BookFilter/BookFilter.js
+++ src/components/BookFilter/BookFilter.js
@@ -1,18 +1,26 @@
 import React from "react";
 import './BookFilter.scss';
+import arrowUp from '../../assets/up-arrow.svg';
+import arrowDown from '../../assets/down__arrow.svg';
 
-const BookFilter = ({filterBooks}) => {
+const BookFilter = ({filterBooks, sortBooks}) => {
     const onSelectChange = (e) =>{
             filterBooks(e.target.value)
     }
     return (
         <div className="book__filter">
-            <select onChange={onSelectChange}>
-                <option value="animals">animals</option>
-                <option value="tourizm">tourizm</option>
-                <option value="food">food</option>
-                <option value="music">music</option>
-            </select>
+            <div>
+                <p onClick={() => sortBooks()} >orderBy</p><img className="arrow" src={arrowDown} color="black" alt="arrowUp" />
+            </div>
+            
+            <div>
+                <select onChange={onSelectChange}>
+                    <option value="animals">animals</option>
+                    <option value="tourizm">tourizm</option>
+                    <option value="food">food</option>
+                    <option value="music">music</option>
+                </select>
+            </div>
         </div>
     )
 }
